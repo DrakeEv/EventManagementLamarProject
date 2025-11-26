0abab76e2002aede7bd0830918758a35a3c5329f
DrakeEv <drakeeeverett@gmail.com>
2025-11-25 19:26:20 -0600

    lil script

---
+++ backend/script.js
@@ -0,0 +1,7 @@
+const addEventButton = document.getElementById("add-event-button");
+
+// tell JS what to do when it's clicked
+addEventButton.addEventListener("click", () => {
+  // this is where your action goes
+  alert("Add event clicked!");
+});
